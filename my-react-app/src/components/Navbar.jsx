74937680f90074d2ad658011f73bf09fb777d032
Dragomir Alexandru <dragomiralexandru153@gmail.com>
2026-01-19 13:28:43 +0200

    Video works
    -edit navbar, footer and home page

--- my-react-app/src/components/Navbar.jsx
+++ my-react-app/src/components/Navbar.jsx
@@ -1,12 +1,15 @@
+import "./Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <nav className="navbar">
-      <Link to="/">Acasă</Link>
-      <Link to="/products">Produse</Link>
-      <Link to="/contact">Contact</Link>
-      <Link to="/cart">Cos</Link>
+      <div className="nav-inner">
+        <Link to="/">Acasă</Link>
+        <Link to="/products">Produse</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/cart">Coș</Link>
+      </div>
     </nav>
   );
 }
--- my-react-app/src/pages/Home.jsx
+++ my-react-app/src/pages/Home.jsx
@@ -5,14 +5,11 @@ function Home() {
     <div className="home-wrapper">
       <div className="home-container">
         <video autoPlay muted loop className="background-video">
-          <source src="/video.mp4" type="video/mp4" />
+          <source src="/video3.mp4" type="video/mp4" />
         </video>
-
-        <div className="home-content">
-          <h1>BimGarage</h1>
-          <p>Custom Car Culture</p>
-        </div>
       </div>
+    
+    
     </div>
   );
 }
